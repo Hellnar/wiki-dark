b3fdd412df33951f065d8f9e49a8635c2f681e0a
StasKlymenko <klymenko.stas@gmail.com>
2023-03-24 21:49:51 +0200

    Reworked styles for old and new versions

---
+++ core.js
@@ -0,0 +1,16 @@
+let url = window.location.href
+checkRedlinks()
+
+document.addEventListener("click", () => {
+    if(window.location.href !== url) {
+        url = window.location.href
+        checkRedlinks()
+    }
+})
+
+function checkRedlinks() {
+    document.querySelectorAll('a[href*="redlink=1"]').forEach(link => {
+        console.log(link)
+        link.classList.add('red-link')
+    })
+}
